0be8ec9f79485d93c191f76a99089487522d03aa
Servando Jose Hernandez Silva <servandoh066@gmail.com>
2026-06-02 11:11:22 +0200

    retoques

--- Version_2/src/main/java/org/example/Juego.java
+++ Version_2/src/main/java/org/example/Juego.java
@@ -66,7 +66,7 @@ public class Juego {
                 actual = enemigo;
                 enemigo = temp;
             } else {
-                System.out.println("Le diste, dale otra");s
+                System.out.println("Le diste, dale otra");
             }
         }
     }
